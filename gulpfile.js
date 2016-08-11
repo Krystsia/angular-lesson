464defa2f25932426ce9d317be4aa884df5da46c
kiryl_krystsia <kiryl_krystsia@epam.com>
2016-08-11 20:18:06 +0300

    add gulp task

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+		sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+		return gulp.src('./styles/scss/*')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./styles/css/'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('styles/scss/*.scss', ['sass']);
+});
