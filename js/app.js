889a9fa732b5a1729116eb2661db81daab781eea
kiryl_krystsia <kiryl_krystsia@epam.com>
2016-08-12 12:47:23 +0300

    add service value

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-(function() {
+(function () {
 	"use strict";
-	
-	 angular.module("angular-lessons", []);
+	var application = angular.module("angular-lessons", []);
+	application.value('model', {
+		user: "Kirill",
+	    userPhoto: '',
+	  	items: [
+		  {action: "Estimate...", done: false},
+		  {action: "Create...", done: false},
+		  {action: "Edit...", done: true},
+		  {action: "Delete..", done: false},
+		]
+	});
+  application.controller("myData", function($scope, model) {
+	$scope.myData = model;
+  })
 })();
