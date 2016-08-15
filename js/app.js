bd7dd7338f652909c68ae8bd0017bdfcb9100edd
Krystsia <krystsia@yandex.ru>
2016-08-15 03:03:24 +0300

    add a few new directives

--- js/app.js
+++ js/app.js
@@ -10,9 +10,26 @@
 		  {action: "Create...", done: false},
 		  {action: "Edit...", done: true},
 		  {action: "Delete..", done: false},
-		]
+		],
+        add: function(action) {
+          this.items.push({action:action, done: false});
+        }
+      
+        
 	});
   application.controller("myData", function($scope, model) {
 	$scope.myData = model;
+    $scope.incompleteCount = incompleteCount;
   })
+  function incompleteCount(items) {
+    var count = 0;
+    
+    angular.forEach(items, function(item) {
+      if(!item.done) count++;
+    });
+    
+    if(count === items.length) return "All incompleted";
+    
+    return count;
+  }
 })();
--- js/app_LOCAL_7220.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	"use strict";
-	
-	 var mainModul = angular.module("angular-lessons", []);
-    mainModul.value('firstModule', {
-      name: 'Kiril',
-      surname: 'Krystsia'
-    })
-})();
