fca11df4ace5e94d96c96c903f1aef0512fe58b3
kiryl_krystsia <kiryl_krystsia@epam.com>
2016-08-15 21:04:25 +0300

    Add some styles from Founddation. Add some behaviors into the he controller

--- js/app.js
+++ js/app.js
@@ -1,35 +1,61 @@
 (function () {
+  
 	"use strict";
 
 	var application = angular.module("angular-lessons", []);
 	application.value('model', {
-		user: "Kirill",
-	    userPhoto: '',
-	  	items: [
-		  {action: "Estimate...", done: false},
-		  {action: "Create...", done: false},
-		  {action: "Edit...", done: true},
-		  {action: "Delete..", done: false},
-		],
-        add: function(action) {
-          this.items.push({action:action, done: false});
-        }
-      
-        
+	  user: "Kirill",
+	  userPhoto: '',
+	  items: [
+		{action: "Estimate...", done: false},
+		{action: "Create...", done: false},
+		{action: "Edit...", done: true},
+		{action: "Delete..", done: false},
+	  ],
+
 	});
-  application.controller("myData", function($scope, model) {
-	$scope.myData = model;
-    $scope.incompleteCount = incompleteCount;
-  })
-  function incompleteCount(items) {
-    var count = 0;
-    
-    angular.forEach(items, function(item) {
-      if(!item.done) count++;
-    });
-    
-    if(count === items.length) return "All incompleted";
-    
-    return count;
-  }
+  
+	application.controller("myData", function($scope, model) {
+		$scope.myData = model;
+
+		//  API
+
+		$scope.incompleteCount = incompleteCount;
+		$scope.warningLevel = warningLevel;
+		$scope.addNewItem = addNewItem;
+	  
+	//	IMP
+	  
+	  
+		
+		
+
+		function incompleteCount(items) {
+			var count = 0;
+
+			angular.forEach(items, function(item) {
+			  if(!item.done) count++;
+			});
+			
+			return count;
+		}
+		
+
+		function warningLevel(items) {
+			return incompleteCount(items) < 3 
+				? "info" 
+				: "warning"
+		}
+		
+		function addNewItem(items, newItem) {
+			if(newItem && newItem.action) {
+				items.push({
+					action: newItem.action,
+					done: false
+				});
+				
+				newItem.action = "";
+			}
+		}
+	})
 })();
