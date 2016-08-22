0706cb2908d790db763b4acd888991411212bc40
kiryl_krystsia <kiryl_krystsia@epam.com>
2016-08-22 19:59:41 +0300

    add json file

--- js/app.js
+++ js/app.js
@@ -5,59 +5,48 @@
 	var application = angular.module("angular-lessons", []);
 	application.value('model', {
 	  user: "Kirill",
-	  userPhoto: '',
-	  items: [
-		{action: "Estimate...", done: false},
-		{action: "Create...", done: false},
-		{action: "Edit...", done: true},
-		{action: "Delete..", done: false},
-	  ],
-
+	  userPhoto: ''
 	});
   
 	application.controller("myData", function($scope, model) {
 		$scope.myData = model;
-
-		//  API
-
 		$scope.incompleteCount = incompleteCount;
 		$scope.warningLevel = warningLevel;
 		$scope.addNewItem = addNewItem;
         $scope.showComplete = true;
-	  
-	//	IMP
-	  
-
-		function incompleteCount(collection) {
-			var count = 0;
-
-			angular.forEach(collection, function(item) {
-			  if(!item.done) count++;
-			});
-			
-			return count;
-		}
-		
-
-		function warningLevel(items) {
-			return incompleteCount(items) < 3 
-				? "info" 
-				: "warning"
-		}
-		
-		function addNewItem(items, newItem) {
-			if(newItem && newItem.action) {
-				items.push({
-					action: newItem.action,
-					done: false
-				});
-				
-				newItem.action = "";
-			}
-		}
 	});
-    
+	
+	application.run(runApp);
     application.filter('checkedItems', checkedItems);
+	
+	function incompleteCount(collection) {
+		var count = 0;
+
+		angular.forEach(collection, function(item) {
+		  if(!item.done) count++;
+		});
+
+		return count;
+	}
+		
+
+	function warningLevel(items) {
+		return incompleteCount(items) < 3 
+			? "info" 
+			: "warning"
+	}
+
+	function addNewItem(items, newItem) {
+		if(newItem && newItem.action) {
+			items.push({
+				action: newItem.action,
+				done: false
+			});
+
+			newItem.action = "";
+		}
+	}
+	
     function checkedItems() {
         return function(items, showComplete){
         var result = [];
@@ -69,4 +58,13 @@
         return result;
         }
     }
+	
+	function runApp($http, model) {
+		$http
+			 .get("main.json")
+			 .then(function(response) {
+			model.items = response.data;
+		});
+	}
+	
 })();
